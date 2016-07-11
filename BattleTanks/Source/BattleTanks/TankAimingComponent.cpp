f4ae7b4b640e95699dbf075053f588abfcef8d23
brolol07 <davis_brock@yahoo.com>
2016-07-10 20:44:32 -0500

    Finish projectile coding

--- BattleTanks/Source/BattleTanks/TankAimingComponent.cpp
+++ BattleTanks/Source/BattleTanks/TankAimingComponent.cpp
@@ -38,6 +38,25 @@ void UTankAimingComponent::TickComponent( float DeltaTime, ELevelTick TickType, 
 }
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed){
-    UE_LOG(LogTemp, Warning, TEXT("Firing at: %f"), LaunchSpeed);
+    if(!Barrel) {return;}
+    
+    FVector OutLaunchVelocity;
+    FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+    if(UGameplayStatics::SuggestProjectileVelocity(
+                                                   this,
+                                                   OutLaunchVelocity,
+                                                   StartLocation,
+                                                   HitLocation,
+                                                   LaunchSpeed,
+                                                   false,
+                                                   0,
+                                                   0,
+                                                   ESuggestProjVelocityTraceOption::DoNotTrace
+                                                   )
+       )
+    {
+        auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+        auto TankName = GetOwner()->GetName();
+    UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *AimDirection.ToString());
+    }
 }
-
